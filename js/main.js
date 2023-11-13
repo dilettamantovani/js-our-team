179e0f205fc93286c1fe9d82d74e4c378cfce06c
dilettits <mantovani.d3@gmail.com>
2023-11-13 08:59:56 +0100

    added html + js: team const

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//definisci oggetto
+//stampa in console
+//trasforma in stringa
+
+const team = [
+    {
+        name: 'Wayne Barnett',
+        title: 'Founder and CEO',
+        picture: 'img/wayne-barnett-founder-and-ceo.jpg'
+    },
+
+    {
+        name: 'Angela Caroll',
+        title: 'Chief Editor',
+        picture: 'img/angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        name: 'Walter Gordon',
+        title: 'Office Manager',
+        picture: 'img/walter-gordon-office-manager.jpg',
+    },
+
+    {
+        name: 'Angela Lopez',
+        title: 'Social Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        name: 'Scott Estrada',
+        title: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg',
+    },
+
+    {
+        name: 'Barbara Ramos',
+        title: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg',
+    },
+]
